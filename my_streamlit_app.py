01861662b8e80eddd0cbd0d23c9c8c97659db52a
ValerieNevo <113360390+ValerieNevo@users.noreply.github.com>
2022-11-23 11:33:01 +0100

    Add files via upload

---
+++ my_streamlit_app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+
+st.title('Hello Wilders, welcome to my application!')
+
+st.write("I enjoy to discover stremalit possibilities")
+
+name = st.text_input("Please give me your name :")
+name_length = len(name)
+st.write("Your name has ",name_length,"characters")
+
+
+link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/weather2019.csv"
+df_weather = pd.read_csv(link)
+st.write(df_weather)
+
+import seaborn as sns
+viz_correlation = sns.heatmap(df_weather.corr(), 
+								center=0,
+								cmap = sns.color_palette("vlag", as_cmap=True)
+								)
+
+st.pyplot(viz_correlation.figure)
